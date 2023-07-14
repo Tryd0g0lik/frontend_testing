9a151361c3d0ddb393653e62c2b5d2ff50cb7718
Tryd0g0lik <work80@mail.ru>
2023-07-14 10:18:22 +0700

    get_cards has been cleaned and litle correct

--- src/jts/mains.ts
+++ src/jts/mains.ts
@@ -1,8 +1,10 @@
 const { CrateForm } = require('./mocks/mock.ts');
-const { NumberCardGenerate } = require('./cards/checkings.ts');
+const { NumberCardGet } = require('./cards/get_cards.ts');
+const { CheckingCards } = require('./cards/checkings.ts');
 
 const elem = new CrateForm();
 elem.changeDOM();
 
-const cards = new NumberCardGenerate(3);
-cards.startWork();
+const cards = new NumberCardGet(3);
+const check = new CheckingCards(cards.startWork());
+check.startWork();
