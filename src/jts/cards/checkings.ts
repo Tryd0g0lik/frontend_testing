f6348e040aabe9c32a389d49ada7dc910d7b836a
Tryd0g0lik <work80@mail.ru>
2023-07-15 11:12:34 +0700

    i can't see result

--- src/jts/cards/checkings.ts
+++ src/jts/cards/checkings.ts
@@ -1,3 +1,4 @@
+const _ = require('underscore.string');
 export class CheckingCards {
 	cards: any;
 	singleCard: [string, number, number] = ['', 0, 0];
@@ -29,21 +30,84 @@ export class CheckingCards {
 		return arr_ofOne_card
 	}
 
-	set setSumIntegOfCardNum(num_: number) {
+	set setIntegOfCardNum(num_: string) {
 		/*
 	 TODO: return tru if a symbol count === integer or false. It integer is some from the 'getOneCards'.
 		:atrib num_: it's a card's number/
 	*/
-
-		const elem = Array(this.getOneCards[2][0])[0];
+		let el = this.getOneCards;
+		console.log("el: ", el)
+		const elem = Array(el[2][0])[0];
+		console.log("elem: ", elem)
 		for (let i = 0; i < elem.length; i++) {
 			if (String(num_).length === elem[i]) {
-				this.singleCard = [this.getOneCards[0], this.getOneCards[1], elem[i]]
+				console.log("sCard--------: ",)
+				this.singleCard = [el[0], el[1], elem[i]]
+
+				return
 			}
 		}
 	}
 
+	startWork(num: string) {
+		/*	
+			TODO:
+			:atrib num: cart's number
+			returns:[<string>, number, number], it's us spearing that we made all true alse ['', 0, 0] :-(
+		*/
+		for (let i = 0; i < this.cards.length; i++) {
+			this.setIntegOfCardNum = String(num)
+			if (this.singleCard[0].length !== 0) this.cards = [];
+		};
+		const sCard = this.singleCard;
+
+		return sCard
+	}
+}
+
+
+export class Luna {
+	manual: [string, number, number];
+	card_num: string;
+	constructor(card_manual: [string, number, number] = ['', 0, 0], card_num: string) {
+		/*
+			TODO: Card's number checking by the Luna-algorithm
+			:atrib card_manual: this's [cart-name, first-integer, symbol count  of number card  ]
+			:atrib card_num: this's cart's number.
+			returns: true or false
+		*/
+		this.manual = card_manual;
+		this.card_num = card_num;
+	}
+
+	get card() {
+		/*Прошел алгоритмя луна или нет */
+
+		const new_man = Array(this.manual)[0].slice();
+		const num = ([this.card_num].slice())[0];
+		console.log("====>> 2", new_man)
+		if (String(new_man[0]).length > 0) {
+			const arr = _.chop(num, 1);
+			let int = 0;
+
+			for (let i = 0; i < Array(arr)[0].length; i = i + 2) {
+				let num = Number(Array(arr)[0][i]) * 2;
+				int += (String(num).length > 2 ? num / 2 : num);
+
+			}
+
+			for (let i = 1; i < Array(arr)[0].length; i++) {
+				let num = (i % 2) !== 0 ? Number(Array(arr)[0][i]) : 0;
+				int += num
+
+			}
+			console.log("====>> 3", int)
+			return (int + 1) % 10 === 0 ? true : false;
+		}
+		return false
+	}
+
 	startWork() {
-		this.setSumIntegOfCardNum = 0;
+		this.card
 	}
 }
--- src/jts/mains.ts
+++ src/jts/mains.ts
@@ -1,10 +1,16 @@
 const { CrateForm } = require('./mocks/mock.ts');
 const { NumberCardGet } = require('./cards/get_cards.ts');
-const { CheckingCards } = require('./cards/checkings.ts');
+const { CheckingCards, Luna } = require('./cards/checkings.ts');
 
 const elem = new CrateForm();
 elem.changeDOM();
 
 const cards = new NumberCardGet(3);
 const check = new CheckingCards(cards.startWork());
-check.startWork();
+// const str = '39313131313131313131313131313131317';
+// const str = '2202201302350075'
+const str = '4276014450895172';
+const manual = check.startWork(str);
+// console.log('manual 2: ', manual)
+const luna = new Luna(manual, str);
+luna.startWork()
